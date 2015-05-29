a4addd3360d3c4cca98a1d7067f2a6416974b159
OLWerdffelinir <werdffelynir@gmail.com>
2015-05-29 15:46:57 +0300

    Initial commit

---
+++ script.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Test JS</title>
+
+	<style rel="stylesheet">
+
+	</style>
+</head>
+<body>
+
+	<div id="box">
+		BOX
+	</div>
+
+
+
+	<script type="text/javascript">
+
+
+	</script>
+
+</body>
+</html>
